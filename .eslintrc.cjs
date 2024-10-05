6a2b162c6c8128773f5db7285f42a7c514ac1882
胡书康 <hushukang@gmail.com>
2024-10-05 19:12:24 +0900

    change cloudfront settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'plugin:prettier/recommended'],
   root: true,
 };
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-import { remixFastify } from "@mcansh/remix-fastify";
-import { fastify } from "fastify";
-import sourceMapSupport from "source-map-support";
+import { remixFastify } from '@mcansh/remix-fastify';
+import { fastify } from 'fastify';
+import sourceMapSupport from 'source-map-support';
 
 sourceMapSupport.install();
 
 const app = fastify();
 
 await app.register(remixFastify);
-
-let address = await app.listen({ port: 3000, host: "0.0.0.0" });
+const desiredPort = Number(process.env.PORT) || 3000;
+let address = await app.listen({ port: desiredPort, host: '0.0.0.0' });
 console.log(`app ready: ${address}`);
