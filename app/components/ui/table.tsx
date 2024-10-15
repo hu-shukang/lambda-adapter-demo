e37c63b96cce0bec015fcf59813aa574e014cc6c
HU SHUKANG <hushukang@gmail.com>
2024-10-16 08:36:50 +0900

    Revert "change tailwind util settings"
    This reverts commit 5f0019418d3ff2961a6dc5e296ddd2ea429b82cb.

--- app/components/ui/table.tsx
+++ app/components/ui/table.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { cn } from '~/.server/utils/tailwind.util';
+
+import { cn } from '~/lib/utils';
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
