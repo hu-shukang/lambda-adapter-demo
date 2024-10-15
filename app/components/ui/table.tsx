5f0019418d3ff2961a6dc5e296ddd2ea429b82cb
HU SHUKANG <hushukang@gmail.com>
2024-10-15 19:41:11 +0900

    change tailwind util settings

--- app/components/ui/table.tsx
+++ app/components/ui/table.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-
-import { cn } from '~/lib/utils';
+import { cn } from '~/.server/utils/tailwind.util';
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
