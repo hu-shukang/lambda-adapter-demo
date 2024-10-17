765a9df8325b771fdf942288ca561a4a50ae0d6c
HU SHUKANG <hushukang@gmail.com>
2024-10-18 08:37:30 +0900

    change callback login

--- app/routes/auth.signin.callback.tsx
+++ app/routes/auth.signin.callback.tsx
@@ -1,15 +1,20 @@
-export default function AuthProviderCallbackPage() {
-  // const submit = useSubmit();
+import { useSubmit } from '@remix-run/react';
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { useEffect } from 'react';
+import { toSignin } from '~/lib/auth.client';
 
-  // useEffect(() => {
-  //   const fetchAuthUser = async () => {
-  //     const authUser = await getCurrentUser();
-  //     if (authUser.userId) {
-  //       toSignin(submit);
-  //     }
-  //   };
-  //   fetchAuthUser();
-  // }, [submit]);
+export default function AuthProviderCallbackPage() {
+  const submit = useSubmit();
+
+  useEffect(() => {
+    const fetchAuthUser = async () => {
+      const authSession = await fetchAuthSession();
+      if (authSession.identityId) {
+        toSignin(submit);
+      }
+    };
+    fetchAuthUser();
+  }, [submit]);
 
   return <div>auth.</div>;
 }
