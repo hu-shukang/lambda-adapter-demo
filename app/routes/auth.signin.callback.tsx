0c1c86773c8d99f35b36dbf4ec5d2e657de916bd
HU SHUKANG <hushukang@gmail.com>
2024-10-18 11:05:00 +0900

    change callback logic

--- app/routes/auth.signin.callback.tsx
+++ app/routes/auth.signin.callback.tsx
@@ -1,3 +1,4 @@
+import 'aws-amplify/auth/enable-oauth-listener';
 import { useSubmit } from '@remix-run/react';
 import { fetchAuthSession } from 'aws-amplify/auth';
 import { useEffect } from 'react';
