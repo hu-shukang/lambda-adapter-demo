df20f25c2d4a9d39b827910cac00fceb09947ba5
HU SHUKANG <hushukang@gmail.com>
2024-10-21 11:51:12 +0900

    remove GlobalSecondaryIndex

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -40,18 +40,18 @@ export class LambdaStack extends cdk.Stack {
     const repository = ecr.Repository.fromRepositoryName(this, `${envs.APP_NAME}-ecr`, envs.APP_NAME);
 
     // 创建一个 DynamoDB 表
-    const userTable = new dynamodb.Table(this, envs.USER_TBL, {
+    new dynamodb.Table(this, envs.USER_TBL, {
       tableName: envs.USER_TBL,
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // 按需计费模式
       removalPolicy: cdk.RemovalPolicy.DESTROY, // 销毁堆栈时销毁表
     });
 
-    userTable.addGlobalSecondaryIndex({
-      indexName: 'ORGANIZATION_PRIORITY_ORDER',
-      partitionKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'priority', type: dynamodb.AttributeType.NUMBER },
-    });
+    // userTable.addGlobalSecondaryIndex({
+    //   indexName: 'ORGANIZATION_PRIORITY_ORDER',
+    //   partitionKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+    //   sortKey: { name: 'priority', type: dynamodb.AttributeType.NUMBER },
+    // });
 
     const commonLayer = new lambda.LayerVersion(this, `${envs.APP_NAME}-common-layer-${envs.ENV}`, {
       layerVersionName: `${envs.APP_NAME}-common-layer-${envs.ENV}`,
