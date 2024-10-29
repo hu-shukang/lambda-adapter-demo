12c28eddbebf151b913204ea40407d1b9d80f45d
HU SHUKANG <hushukang@gmail.com>
2024-10-29 20:02:42 +0900

    change pre token login

--- lambda/functions/cognito-triggers/pre-token/index.ts
+++ lambda/functions/cognito-triggers/pre-token/index.ts
@@ -1,11 +1,31 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
 import { PreTokenGenerationTriggerEvent } from 'aws-lambda';
+
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event: PreTokenGenerationTriggerEvent): Promise<any> => {
   console.log('Event: ', JSON.stringify(event, null, 2));
-
-  event.response.claimsOverrideDetails = {
-    claimsToAddOrOverride: {},
-  };
+  const { userName } = event;
+  const command = new GetCommand({
+    TableName: process.env.USER_TBL!,
+    Key: {
+      pk: userName,
+      sk: 'USER_INFO',
+    },
+  });
+  const dbResult = await ddbDocClient.send(command);
+  const user = dbResult.Item;
+  if (user) {
+    event.response.claimsOverrideDetails = {
+      claimsToAddOrOverride: {
+        organization: user.organization,
+        status: user.status,
+        name: user.name,
+      },
+    };
+  }
 
   return event;
 };
