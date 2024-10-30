e0b82c39743878b320802e5572c04bc1d96aa2b1
HU SHUKANG <hushukang@gmail.com>
2024-10-30 15:00:29 +0900

    change cognito trigger lambda

--- lambda/functions/cognito-triggers/post-confirmation/index.ts
+++ lambda/functions/cognito-triggers/post-confirmation/index.ts
@@ -1,17 +1,23 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { PostConfirmationTriggerEvent } from 'aws-lambda';
+import {
+  AdminGetUserCommand,
+  AdminGetUserCommandOutput,
+  AdminLinkProviderForUserCommand,
+  AttributeType,
+  CognitoIdentityProviderClient,
+} from '@aws-sdk/client-cognito-identity-provider';
 
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
+const cognitoClient = new CognitoIdentityProviderClient({});
 
-export const handler = async (event: PostConfirmationTriggerEvent): Promise<any> => {
-  console.log('Event: ', JSON.stringify(event, null, 2));
+const insertUserToDB = async (event: PostConfirmationTriggerEvent) => {
   const {
     userName,
     request: { userAttributes },
   } = event;
-
   const updateExpressionList = ['#email = :email', '#sub = :sub'];
   const expressionAttributeNames: Record<string, string> = {
     '#email': 'email',
@@ -39,6 +45,54 @@ export const handler = async (event: PostConfirmationTriggerEvent): Promise<any>
   });
 
   await ddbDocClient.send(command);
+};
+
+const getUserFromCognito = async (event: PostConfirmationTriggerEvent) => {
+  const {
+    request: { userAttributes },
+  } = event;
+  const { email } = userAttributes;
+  const command = new AdminGetUserCommand({
+    UserPoolId: process.env.USER_POOL_ID,
+    Username: email,
+  });
+  return await cognitoClient.send(command);
+};
+
+const getProviderName = (userAttribute: AttributeType[] | undefined) => {
+  let providerName = 'Cognito';
+  const existingUserIdentities = userAttribute?.find((a) => a.Name === 'identities');
+  if (existingUserIdentities && existingUserIdentities.Value) {
+    providerName = existingUserIdentities.Value;
+  }
+  return providerName;
+};
+
+const linkUser = async (event: PostConfirmationTriggerEvent, existingUser: AdminGetUserCommandOutput) => {
+  const sourceUserProviderName = getProviderName(existingUser.UserAttributes);
+  const linkProviderCommand = new AdminLinkProviderForUserCommand({
+    UserPoolId: process.env.USER_POOL_ID,
+    DestinationUser: {
+      ProviderAttributeValue: existingUser.Username,
+      ProviderName: 'Cognito',
+    },
+    SourceUser: {
+      ProviderName: sourceUserProviderName,
+    },
+  });
+  await cognitoClient.send(linkProviderCommand);
+};
+
+export const handler = async (event: PostConfirmationTriggerEvent): Promise<any> => {
+  console.log('Event: ', JSON.stringify(event, null, 2));
+
+  const existingUser = await getUserFromCognito(event);
+  if (existingUser) {
+    await linkUser(event, existingUser);
+  } else {
+    await insertUserToDB(event);
+  }
+
   console.log('User data saved successfully');
 
   return event;
