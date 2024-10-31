c764bc99aee4d4222e501325324bc0461196bb66
HU SHUKANG <hushukang@gmail.com>
2024-10-31 11:01:56 +0900

    change package lamdbas shell

--- package-lambdas.sh
+++ package-lambdas.sh
@@ -8,7 +8,7 @@ TIMESTAMP=$2
 LAMBDA_DIST_DIR="${CODEBUILD_SRC_DIR}/lambda/dist"
 
 # 遍历 lambda/dist 目录下的所有文件夹并进行打包和上传
-for lambda_dir in ${LAMBDA_DIST_DIR}/*; do
+for lambda_dir in $(find "${LAMBDA_DIST_DIR}" -type d -mindepth 2 -maxdepth 2); do
   if [ -d "$lambda_dir" ]; then
     lambda_name=$(basename "$lambda_dir")
     echo "Processing $lambda_name..."
