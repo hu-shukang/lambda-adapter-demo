ded603286a597d372002b7ac110bd38444dadd78
HU SHUKANG <hushukang@gmail.com>
2024-10-31 16:43:48 +0900

    change custom resource lambda

--- lambda/functions/custom-resource/update-cognito-trigger/index.ts
+++ lambda/functions/custom-resource/update-cognito-trigger/index.ts
@@ -3,30 +3,43 @@ import {
   LambdaConfigType,
   UpdateUserPoolCommand,
 } from '@aws-sdk/client-cognito-identity-provider';
-import { CdkCustomResourceEvent, Context } from 'aws-lambda';
+import {
+  CloudFormationCustomResourceEvent,
+  CloudFormationCustomResourceFailedResponse,
+  CloudFormationCustomResourceSuccessResponse,
+  Context,
+} from 'aws-lambda';
 
 const cognitoClient = new CognitoIdentityProviderClient({});
 
-export const handler = async (event: CdkCustomResourceEvent, context: Context) => {
+export const handler = async (event: CloudFormationCustomResourceEvent, context: Context) => {
   console.log('Event: ', JSON.stringify(event, null, 2));
   const lambdaConfig = event.ResourceProperties.lambdaConfig as LambdaConfigType;
   const command = new UpdateUserPoolCommand({
     UserPoolId: process.env.USER_POOL_ID,
     LambdaConfig: event.RequestType === 'Delete' ? undefined : lambdaConfig,
   });
-  try {
-    const output = await cognitoClient.send(command);
-    console.log(output);
 
-    return {
+  try {
+    await cognitoClient.send(command);
+    const response: CloudFormationCustomResourceSuccessResponse = {
+      StackId: event.StackId,
+      RequestId: event.RequestId,
+      LogicalResourceId: event.LogicalResourceId,
+      PhysicalResourceId: context.logGroupName,
       Status: 'SUCCESS',
-      PhysicalResourceId: context.logGroupName,
     };
-  } catch (e) {
+    return response;
+  } catch (e: any) {
     console.log(e);
-    return {
-      Status: 'FAILED',
+    const response: CloudFormationCustomResourceFailedResponse = {
+      StackId: event.StackId,
+      RequestId: event.RequestId,
+      LogicalResourceId: event.LogicalResourceId,
       PhysicalResourceId: context.logGroupName,
+      Status: 'FAILED',
+      Reason: e.message || 'custom resource fail',
     };
+    return response;
   }
 };
