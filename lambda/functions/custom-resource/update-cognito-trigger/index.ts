bc1e598ee2a18a6ef05466f7a719cf2c0d6730b2
HU SHUKANG <hushukang@gmail.com>
2024-10-31 20:34:45 +0900

    change custom resource lambda

--- lambda/functions/custom-resource/update-cognito-trigger/index.ts
+++ lambda/functions/custom-resource/update-cognito-trigger/index.ts
@@ -3,14 +3,55 @@ import {
   LambdaConfigType,
   UpdateUserPoolCommand,
 } from '@aws-sdk/client-cognito-identity-provider';
-import {
-  CloudFormationCustomResourceEvent,
-  CloudFormationCustomResourceFailedResponse,
-  CloudFormationCustomResourceSuccessResponse,
-  Context,
-} from 'aws-lambda';
+import { CloudFormationCustomResourceEvent, Context } from 'aws-lambda';
+import * as https from 'https';
 
 const cognitoClient = new CognitoIdentityProviderClient({});
+
+const sendResponse = async (
+  event: CloudFormationCustomResourceEvent,
+  context: Context,
+  status: 'SUCCESS' | 'FAILED',
+  reason: string,
+) => {
+  const responseBody = JSON.stringify({
+    Status: status,
+    Reason: reason,
+    PhysicalResourceId: context.logGroupName,
+    StackId: event.StackId,
+    RequestId: event.RequestId,
+    LogicalResourceId: event.LogicalResourceId,
+  });
+
+  const parsedUrl = new URL(event.ResponseURL);
+  const options = {
+    hostname: parsedUrl.hostname,
+    port: 443,
+    path: parsedUrl.pathname + parsedUrl.search,
+    method: 'PUT',
+    headers: {
+      'Content-Type': '',
+      'Content-Length': responseBody.length,
+    },
+  };
+
+  return new Promise<boolean>((resolve, reject) => {
+    const request = https.request(options, (response) => {
+      console.log(`Status Code: ${response.statusCode}`);
+      response.statusCode === 200
+        ? resolve(true)
+        : reject(new Error(`Failed to send response: ${response.statusCode}`));
+    });
+
+    request.on('error', (error) => {
+      console.error('sendResponse Error:', error);
+      reject(error);
+    });
+
+    request.write(responseBody);
+    request.end();
+  });
+};
 
 export const handler = async (event: CloudFormationCustomResourceEvent, context: Context) => {
   console.log('Event: ', JSON.stringify(event, null, 2));
@@ -22,24 +63,9 @@ export const handler = async (event: CloudFormationCustomResourceEvent, context:
 
   try {
     await cognitoClient.send(command);
-    const response: CloudFormationCustomResourceSuccessResponse = {
-      StackId: event.StackId,
-      RequestId: event.RequestId,
-      LogicalResourceId: event.LogicalResourceId,
-      PhysicalResourceId: context.logGroupName,
-      Status: 'SUCCESS',
-    };
-    return response;
+    await sendResponse(event, context, 'SUCCESS', 'Update successful');
   } catch (e: any) {
     console.log(e);
-    const response: CloudFormationCustomResourceFailedResponse = {
-      StackId: event.StackId,
-      RequestId: event.RequestId,
-      LogicalResourceId: event.LogicalResourceId,
-      PhysicalResourceId: context.logGroupName,
-      Status: 'FAILED',
-      Reason: e.message || 'custom resource fail',
-    };
-    return response;
+    await sendResponse(event, context, 'FAILED', e.message || 'Custom resource failed');
   }
 };
