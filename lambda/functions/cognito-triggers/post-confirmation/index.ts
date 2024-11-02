a21cfb890f4752c7eb5c5f74035e5d1a4777d350
胡书康 <hushukang@gmail.com>
2024-11-02 15:21:43 +0900

    change post confirmation lambda

--- lambda/functions/cognito-triggers/post-confirmation/index.ts
+++ lambda/functions/cognito-triggers/post-confirmation/index.ts
@@ -1,9 +1,11 @@
+import { CognitoIdentityProviderClient, ListUsersCommand } from '@aws-sdk/client-cognito-identity-provider';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { PostConfirmationTriggerEvent } from 'aws-lambda';
 
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
+const cognitoClient = new CognitoIdentityProviderClient({});
 
 const insertUserToDB = async (event: PostConfirmationTriggerEvent) => {
   const {
@@ -39,9 +41,25 @@ const insertUserToDB = async (event: PostConfirmationTriggerEvent) => {
   await ddbDocClient.send(command);
 };
 
+const getUsersByEmail = async (event: PostConfirmationTriggerEvent) => {
+  const {
+    request: { userAttributes },
+  } = event;
+  const { email } = userAttributes;
+  const listUsersParams = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Filter: `email = "${email}"`,
+  };
+  const listUsersCommand = new ListUsersCommand(listUsersParams);
+  const output = await cognitoClient.send(listUsersCommand);
+  return output.Users;
+};
+
 export const handler = async (event: PostConfirmationTriggerEvent): Promise<any> => {
   console.log('Event: ', JSON.stringify(event, null, 2));
 
+  const users = await getUsersByEmail(event);
+  console.log(users);
   await insertUserToDB(event);
   console.log('User data saved successfully');
 
