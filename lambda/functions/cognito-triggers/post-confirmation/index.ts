711f2b3626e31a8c8f19554e1be7aef6814f161c
胡书康 <hushukang@gmail.com>
2024-11-04 21:39:02 +0900

    change post confirmation lambda

--- lambda/functions/cognito-triggers/post-confirmation/index.ts
+++ lambda/functions/cognito-triggers/post-confirmation/index.ts
@@ -1,6 +1,10 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { PostConfirmationTriggerEvent } from 'aws-lambda';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
@@ -10,17 +14,22 @@ const insertUserToDB = async (event: PostConfirmationTriggerEvent) => {
     userName,
     request: { userAttributes },
   } = event;
+  // updateTime: dateUtil.utc(), updateUser: payload['cognito:username']
   const updateExpressionList = [
     '#userName = :userName',
     '#sub = :sub',
     '#cognitoUserStatus = :cognitoUserStatus',
     '#organization = :organization',
+    '#updateTime = :updateTime',
+    '#updateUser = :updateUser',
   ];
   const expressionAttributeNames: Record<string, string> = {
     '#userName': 'userName',
     '#sub': 'sub',
     '#cognitoUserStatus': 'cognitoUserStatus',
     '#organization': 'organization',
+    '#updateTime': 'updateTime',
+    '#updateUser': 'updateUser',
   };
   let cognitoUserStatus = userAttributes['cognito:user_status'];
   if (userAttributes.identities) {
@@ -32,6 +41,8 @@ const insertUserToDB = async (event: PostConfirmationTriggerEvent) => {
     ':sub': userAttributes.sub,
     ':cognitoUserStatus': cognitoUserStatus,
     ':organization': 'DEFAULT',
+    ':updateTime': dayjs().utc().toISOString(),
+    ':updateUser': userAttributes.email,
   };
   if (userAttributes.name) {
     updateExpressionList.push('#name = :name');
