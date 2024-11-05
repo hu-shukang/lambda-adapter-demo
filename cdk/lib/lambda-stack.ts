861646109012b7f55fa34df60560cd380841c186
HU SHUKANG <hushukang@gmail.com>
2024-11-05 22:46:35 +0900

    add permission to logWriteLambda

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -130,6 +130,11 @@ export class LambdaStack extends cdk.Stack {
       ...lambdaProps,
     });
 
+    logWriteLambda.addPermission('AllowSQSInvoke', {
+      principal: new iam.ServicePrincipal('sqs.amazonaws.com'),
+      sourceArn: logQueue.queueArn,
+    });
+
     logWriteLambda.addEventSource(
       new SqsEventSource(logQueue, {
         batchSize: 10, // 每次最多处理的消息数量
