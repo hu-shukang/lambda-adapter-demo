075e22d700213267fc4fc4fa6e5aa9dee1db33e5
HU SHUKANG <hushukang@gmail.com>
2024-11-12 16:04:34 +0900

    change lambda package settings

---
+++ lambda/functions/log-write/index.ts
@@ -0,0 +1,9 @@
+import { SQSEvent } from 'aws-lambda';
+
+export const handler = async (event: SQSEvent) => {
+  console.log('Event: ', JSON.stringify(event, null, 2));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Log recorded successfully' }),
+  };
+};
